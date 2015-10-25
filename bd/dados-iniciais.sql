defc6f61407a3b52604a10d101b564cd305cf215
Alisson Chiquitto <chiquitto@gmail.com>
2015-10-25 12:23:18 -0200

    Scripts para o banco de dados

---
+++ bd/dados-iniciais.sql
@@ -0,0 +1,16 @@
+
+
+INSERT INTO `posts` (`title`, `description`) VALUES ('Post 1', 'Descricao 1');
+INSERT INTO `posts` (`title`, `description`) VALUES ('Post 2', 'Descricao 2');
+INSERT INTO `posts` (`title`, `description`) VALUES ('Post 3', 'Descricao 3');
+INSERT INTO `posts` (`title`, `description`) VALUES ('Post 4', 'Descricao 4');
+INSERT INTO `posts` (`title`, `description`) VALUES ('Post 5', 'Descricao 5');
+INSERT INTO `posts` (`title`, `description`) VALUES ('Post 6', 'Descricao 6');
+INSERT INTO `posts` (`title`, `description`) VALUES ('Post 7', 'Descricao 7');
+INSERT INTO `posts` (`title`, `description`) VALUES ('Post 8', 'Descricao 8');
+INSERT INTO `posts` (`title`, `description`) VALUES ('Post 9', 'Descricao 9');
+INSERT INTO `posts` (`title`, `description`) VALUES ('Post 10', 'Descricao 10');
+INSERT INTO `posts` (`title`, `description`) VALUES ('Post 11', 'Descricao 11');
+INSERT INTO `posts` (`title`, `description`) VALUES ('Post 12', 'Descricao 12');
+INSERT INTO `posts` (`title`, `description`) VALUES ('Post 13', 'Descricao 13');
+
